88eaf10823c8d8d20e7a933d05604cdccb8c25de
gimisu <xsuex12@naver.com>
2021-07-28 00:03:57 +0900

    +css

--- js/background.js
+++ js/background.js
@@ -2,8 +2,13 @@ const images = [
     "몬스터대학교.jpg",
     "소울.jpg",
     "알라딘.jpg",
+    "토이스토리.jpg",
+    "디즈니성.jpg",
+    "라이언킹.jpg",
     "코코.jpg",
-    "토이스토리.jpg"
+    "미녀와야수.jpg",
+    "푸.jpg",
+    "알라딘2.jpg"
 ];
 
 const chosenImage = images[Math.floor(Math.random()*images.length)];
@@ -11,4 +16,4 @@ const chosenImage = images[Math.floor(Math.random()*images.length)];
 const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`;
-document.body.appendChild(bgImage);
+document.body.prepend(bgImage);
--- js/weather.js
+++ js/weather.js
@@ -1,13 +1,21 @@
-const API_KEY = "ad4f253135535132568cf739a1acecc5";
+const API_KEY = "ad4f253135555132568cf739a1acecc5";
 
 function onGeoOk(position) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    const url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    fetch(url);
-}
-  function onGeoError() {
-    alert("Can't find you. No weather for you.");
-  }
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
   
-  navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    const weather = document.querySelector("#weather span:first-child");
+    const city = document.querySelector("#weather span:last-child");
+    city.innerText = data.name;
+    weather.innerText = `🌥 ${data.weather[0].main} / ${data.main.temp}℃ `;
+});
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.");
+}
+  
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
